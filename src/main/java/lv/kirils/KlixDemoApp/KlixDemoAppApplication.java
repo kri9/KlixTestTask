336618d6a3df8da0c6b8604009c78a9f758abd45
KillReal2418 <therealapplegame@gmail.com>
2025-07-01 18:35:53 +0300

    init

---
+++ src/main/java/lv/kirils/KlixDemoApp/KlixDemoAppApplication.java
@@ -0,0 +1,13 @@
+package lv.kirils.KlixDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlixDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlixDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/kirils/KlixDemoApp/KlixDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.kirils.KlixDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlixDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
